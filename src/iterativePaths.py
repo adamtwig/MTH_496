134f5b0f859e443a2eff1b25aa2cda458e9bf735
Adam Terwilliger <adamtwig@hotmail.com>
2016-12-15 22:56:23 -0500

    cleaned up code - added documentation

--- src/iterativePaths.py
+++ src/iterativePaths.py
@@ -11,13 +11,13 @@ import itertools as it
 import numpy as np
 import sys
 
-
+# print options allow for long lines to print
 np.set_printoptions(linewidth=1000)
 np.set_printoptions(threshold='nan')
 
-
 def main():
 
+	# user inputs length of path (#U + #S)
 	if ( len(sys.argv) != 2 ):
 		print 'program parameters incorrect'
 		print 'usage: ./prog.py size '
@@ -26,66 +26,80 @@ def main():
 		exampleKey = int(sys.argv[1])
 
 
+	# only operations are up, down, straight
 	operations = ['U','D','S']
 
-	#maxPathLen = 15
-	#maxPathLen = 10
-
+	# max string length is twice the path size (all UD steps)
 	maxPathLen = 2 * exampleKey + 1
 
+	# initialize data structures
 	possiblePaths = []
-
 	pathDict = {}
 	lexPathDict = {}
 
+	# generate all possible strings
 	for m in range(maxPathLen):
 		possiblePaths = possiblePaths + [''.join(i) for i in it.product(operations, repeat = m)]
 
+	# filter out only paths we are interested in
 	for path in possiblePaths:
 	
 		if isValidPath(path):
 			size = getPathSize(path)
 
+			# key to dict is size (collect all paths for specific size)
 			if size not in pathDict:
 				pathDict[size] = [path]
 			else:
 				pathDict[size].append(path)
 
+	# if desire to print all paths
 	#for key in pathDict:
 		#print key, pathDict[key], len(pathDict[key])
 		#print key, len(pathDict[key])
 
-	#exampleKey = 4
 	lexList = []
 	edgeList = []
+	# map paths to lexiographic notation 
 	for path in pathDict[exampleKey]:
-		#print exampleKey, path, lexMapPath(path) 
 		pathEdgeList = ruleStoDU(path) + ruleUDtoS(path)
 		for vertex in pathEdgeList:
 			edgeList.append((lexMapPath(path),vertex))	
 		lexList.append(lexMapPath(path))
 
+	# sort paths based on lexiographic ordering
 	sLexList = sorted(lexList)
 
+	# initialize adj matrix to all 0s
 	lexMatrix = np.zeros([len(lexList),len(lexList)], dtype=np.int)
 
-	#for path in sLexList:
-	#	print path, sLexList.index(path)
-
+	# get vertex indices and set path between them
 	for i in range(len(edgeList)):
 		vertex0index = sLexList.index(edgeList[i][0])
 		vertex1index = sLexList.index(edgeList[i][1])
-		#print edgeList[i], vertex0index, vertex1index
 		lexMatrix[vertex0index,vertex1index] = 1
 
-
+	# get diff between identity and adj matrix
 	diffIandX = np.identity(len(lexList)) - lexMatrix
 
+	# take inverse of matrix (we know it's invertibile
+	#	because diagonal is all 1s and lower triangle is all 0s
 	lexInvMatrix = np.linalg.inv(diffIandX).astype(np.int)
 
+	# get column and row sums for (I-X)^-1
 	colSums = np.sum(lexInvMatrix, axis=0).tolist()
 	rowSums = np.sum(lexInvMatrix, axis=1).tolist()
+	
+	print 'Dimensions of Matrix:', lexMatrix.shape
 
+	print 'Column Sums of (I-X)^-1:',np.sum(lexInvMatrix, axis=0)
+	print 'Row Sums of (I-X)^-1:',np.sum(lexInvMatrix, axis=1)
+	
+	# print adj matrix and (I-X)^-1 matrix
+	print lexMatrix
+	print lexInvMatrix
+
+	'''
 	dupColSums = set([x for x in colSums if colSums.count(x) > 1])
 	dupRowSums = set([x for x in rowSums if rowSums.count(x) > 1])
 
@@ -101,6 +115,7 @@ def main():
 		if freqRowSums[i][1] == 2:
 			frs+=1
 
+	# collect stats about row/col sums
 	print "Size:", exampleKey
 
 	print "Num colSums:", len(colSums)
@@ -119,63 +134,18 @@ def main():
 	print "rowSums:", rowSums
 	print "dupColSums:", dupColSums
 	print "dupRowSums:", dupRowSums
-
-	#print 'Dimensions of Matrix:', lexMatrix.shape
-
-	#print 'Column Sums of Original:', np.sum(lexMatrix, axis=0)
-	#print 'Row Sums of Original:', np.sum(lexMatrix, axis=1)
-
-	#print 'Column Sums of (I-X)^-1:',np.sum(lexInvMatrix, axis=0)
-	#print 'Row Sums of (I-X)^-1:',np.sum(lexInvMatrix, axis=1)
-
-	#print lexMatrix
-	#print lexInvMatrix
-
-	#examplePath = 'SUUUSSSDDDS'
-
-	#print ruleStoDU(examplePath)
-	#print ruleUDtoS(examplePath)
-
-	'''
-	for i in range(len(examplePath)):
-		newPath = examplePath
-		if examplePath[i] == 'S':
-			newPath = examplePath[:i] + 'DU' + examplePath[i+1:]
-			print i
-			print 'Original:', examplePath, isValidPath(examplePath), getPathSize(examplePath)
-			print 'New:     ', newPath, isValidPath(newPath), getPathSize(newPath)
-			print '\n'
-	
-	Ulist = []
-	for i in range(len(examplePath)):
-		newPath = examplePath
-		if examplePath[i] == 'U':
-			Ulist.append(i)
-		if examplePath[i] == 'D':
-			currU = Ulist.pop()
-			currD = i
-			stringBeforeU = examplePath[:currU]
-			stringBetweenUandD = examplePath[currU+1:currD]
-			stringAfterD = examplePath[currD+1:]
-
-			newPathBefore = stringBeforeU + 'S' + stringBetweenUandD + stringAfterD
-			newPathAfter = stringBeforeU + stringBetweenUandD + 'S' + stringAfterD
-
-			print currU, currD
-			print 'Original:  ', examplePath, isValidPath(examplePath), getPathSize(examplePath)
-			#if newPathBefore == newPathAfter:
-			#	print newPathBefore, isValidPath(newPathBefore), getPathSize(examplePath)
-			#else:
-			print 'New Before:', newPathBefore, isValidPath(newPathBefore), getPathSize(examplePath)
-			print 'New After: ', newPathAfter, isValidPath(newPathAfter), getPathSize(examplePath)
-			print '\n'
-
-		print examplePath, lexMapPath(examplePath)
 	'''
 	
 def isValidPath(myPath):
+	"""
+	@summary: returns 0/1 (True/False) if path is valid
+		-- makes sure path is balanced with U and D steps
+		-- makes sure path does not go below x axis
+	"""
+
 	unbalanced = 0
 	belowAxis = 0
+	valid = 0
 	for char in myPath:
 		if char == 'U':
 			unbalanced +=1
@@ -185,22 +155,28 @@ def isValidPath(myPath):
 			belowAxis = unbalanced
 
 	if (unbalanced == 0) and (belowAxis == 0):
-		#print path, belowAxis, unbalanced, size
 		valid = 1
-	else:
-		valid = 0
 
 	return valid
 
 
 def getPathSize(myPath):
+	"""
+	@summary: returns size of path
+		-- number of up steps + straight steps
+	"""
 	size = 0
 	for char in myPath:
 		if char == 'U' or char == 'S':
 			size += 1
 	return size
 
+
 def lexMapPath(myPath):
+	"""
+	@summary: map path to lexiographically 
+				ordered version
+	"""
 	newPath = ''
 	myMap = {'U':'a', 'S':'b', 'D':'c'}
 	for i in range(len(myPath)):
@@ -208,6 +184,9 @@ def lexMapPath(myPath):
 	return newPath
 
 def ruleStoDU(myPath):
+	"""
+	@summary: turns straight steps into down-up
+	"""
 	StoDUlist = []
 	for i in range(len(myPath)):
 		if myPath[i] == 'S':
@@ -218,12 +197,17 @@ def ruleStoDU(myPath):
 	return StoDUlist
 	
 def ruleUDtoS(myPath):
+	"""
+	@summary: turns up-down pairs into straight steps
+	"""
 	UDtoSlist = []
 	Ulist = []
 	for i in range(len(myPath)):
 		newPath = myPath
+		# create a list of where up steps are
 		if myPath[i] == 'U':
 			Ulist.append(i)
+		# as you find down steps, pop off corresponding up steps
 		if myPath[i] == 'D':
 			currU = Ulist.pop()
 			currD = i
@@ -231,6 +215,7 @@ def ruleUDtoS(myPath):
 			stringBetweenUandD = myPath[currU+1:currD]
 			stringAfterD = myPath[currD+1:]
 
+			# create new path by adding straight step to left of remaining path 
 			newPathBefore = stringBeforeU + 'S' + stringBetweenUandD + stringAfterD
 			newPathAfter = stringBeforeU + stringBetweenUandD + 'S' + stringAfterD
 
